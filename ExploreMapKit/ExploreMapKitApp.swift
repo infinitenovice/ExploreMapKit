55ee0376ad320ccd5b37885398c664f95e1c3f35
DRSaz <schmidliaz@gmail.com>
2024-06-07 10:37:57 -0700

    Initial Commit

---
+++ ExploreMapKit/ExploreMapKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreMapKitApp.swift
+//  ExploreMapKit
+//
+//  Created by Infinite Novice on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ExploreMapKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
